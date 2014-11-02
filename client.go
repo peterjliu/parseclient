88a3a7ed4da3cb4581d764a2972f066d3fdc5742
peterliu <peterjliu@gmail.com>
2014-11-01 18:06:03 -0700

    added Headers() method to Client

--- client.go
+++ client.go
@@ -19,15 +19,19 @@ func New(id, key string) *Client {
 	return c
 }
 
+func (c Client) Headers() map[string]string {
+	return map[string]string{
+		"X-Parse-Application-Id": c.AppId,
+		"X-Parse-REST-API-Key":   c.ApiKey,
+	}
+}
+
 // Get Parse object and fill in struct
 func (c Client) GetObj(class, id string, out interface{}) error {
 	r := rest.Request{
-		Method: rest.GET,
-		Headers: map[string]string{
-			"X-Parse-Application-Id": c.AppId,
-			"X-Parse-REST-API-Key":   c.ApiKey,
-		},
-		Url: fmt.Sprintf("https://api.parse.com/1/classes/%s/%s", class, id),
+		Method:  rest.GET,
+		Headers: c.Headers(),
+		Url:     fmt.Sprintf("https://api.parse.com/1/classes/%s/%s", class, id),
 	}
 	err := r.Do(&out)
 	if err != nil {
@@ -40,11 +44,12 @@ func (c Client) GetObj(class, id string, out interface{}) error {
 // Get Parse object, without specifying type
 func (c Client) GetObjMap(class, id string) (map[string]interface{}, error) {
 	var v interface{}
-	err := rest.Get(fmt.Sprintf("https://api.parse.com/1/classes/%s/%s", class, id),
-		map[string]string{
-			"X-Parse-Application-Id": c.AppId,
-			"X-Parse-REST-API-Key":   c.ApiKey,
-		}, &v)
+	r := rest.Request{
+		Method:  rest.GET,
+		Headers: c.Headers(),
+		Url:     fmt.Sprintf("https://api.parse.com/1/classes/%s/%s", class, id),
+	}
+	err := r.Do(&v)
 	if err != nil {
 		fmt.Println("Failed to get object")
 		return nil, err
